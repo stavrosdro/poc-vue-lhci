9263f6b5c95343b78f764de7b1355abb7ef7ed00
Droutsas, Stavros <stavrosdro@gmail.com>
2023-05-02 19:28:26 +0300

    added github workflows

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,19 +1,20 @@
 module.exports = {
-    ci: {
-      assert: {
-        assertions: {
-            "categories:accessibility": ["error", {"minScore": 0.8}]
-        }
-      },
-      collect: {
-        /* Add configuration here */
-        startServerCommand: 'npm run serve',
-        url: ['http://localhost:8080'],
-        numberOfRuns: 5
-      },
-      upload: {
-        /* Add configuration here */
-        target: 'temporary-public-storage',
-      },
+  ci: {
+    assert: {
+      assertions: {
+        "categories:performance": ['warn', {minScore: 0.8}],
+        "categories:accessibility": ["error", {"minScore": 0.8}],
+      }
     },
-  };
+    collect: {
+      /* Add configuration here */
+      startServerCommand: 'npm run serve',
+      url: ['http://localhost:8080'],
+      numberOfRuns: 3
+    },
+    upload: {
+      /* Add configuration here */
+      target: 'temporary-public-storage',
+    },
+  },
+};
